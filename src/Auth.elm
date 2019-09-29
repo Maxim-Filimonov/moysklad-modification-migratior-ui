bf8fbaf022bd711d4dc6347d850f6a337421f149
Maxim Filimonov <tpaktopsp@gmail.com>
2019-09-29 20:31:25 +0300

    add call to http api on login

--- src/Auth.elm
+++ src/Auth.elm
@@ -4,29 +4,81 @@ import Base64
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (attribute, class, disabled, href, placeholder)
 import Html.Styled.Events exposing (onClick, onInput)
+import Http
+import RemoteData exposing (RemoteData, WebData)
 
 
 type alias Model =
     { username : String
     , password : String
+    , loginResponse : WebData JwtToken
     }
 
 
-buildAuthorizationToken : Model -> String
-buildAuthorizationToken model =
+buildAuthorizationHeader : Model -> String
+buildAuthorizationHeader model =
     "Basic " ++ Base64.encode (model.username ++ ":" ++ model.password)
+
+
+type alias JwtToken =
+    String
+
+
+type LoginResponse
+    = WebData JwtToken
+
+
+loginRequest : Model -> Cmd Msg
+loginRequest model =
+    Http.request
+        { method = "POST"
+        , headers = [ Http.header "Authorization" <| buildAuthorizationHeader model ]
+        , expect = Http.expectString (RemoteData.fromResult >> GotLoginResponse)
+        , timeout = Nothing
+        , tracker = Nothing
+        , url = "https://reify-modification-migrator.builtwithdark.com/login"
+        , body = Http.emptyBody
+        }
 
 
 type Msg
     = ChangePassword String
     | ChangeLogin String
     | Submit
+    | GotLoginResponse (WebData JwtToken)
 
 
 view : Model -> List (Html Msg)
 view model =
+    let
+        requestText =
+            case model.loginResponse of
+                RemoteData.Loading ->
+                    "Загружаеться"
+
+                RemoteData.NotAsked ->
+                    ""
+
+                RemoteData.Success jwt ->
+                    "Логин успешен:" ++ jwt
+
+                RemoteData.Failure error ->
+                    case error of
+                        Http.BadStatus status ->
+                            if status == 403 then
+                                "Неправильный логин или пароль"
+
+                            else
+                                "Ошибка статуса " ++ String.fromInt status
+
+                        _ ->
+                            "Неизвестная ошибка"
+    in
     [ div [ class "flex w-full justify-center" ]
-        [ form [ class "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" ]
+        [ form
+            [ class "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            , Html.Styled.Events.onSubmit Submit
+            ]
             [ div [ class "mb-4" ]
                 [ label []
                     [ span [ class "block text-gray-700 text-sm font-bold mb-2" ] [ text "Логин" ]
@@ -46,13 +98,20 @@ view model =
                     , input
                         [ class "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         , placeholder "пароль"
-                        , Html.Styled.Attributes.type_ "text"
+                        , Html.Styled.Attributes.type_ "password"
                         , Html.Styled.Attributes.value model.password
                         , onInput ChangePassword
                         ]
                         []
                     ]
                 ]
+            , input
+                [ class "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                , Html.Styled.Attributes.type_ "submit"
+                , Html.Styled.Attributes.value "Логин"
+                ]
+                []
+            , div [ class "mt-4 text-red-500" ] [ text requestText ]
             ]
         ]
     ]
@@ -65,12 +124,15 @@ update msg model =
             ( { model | username = login }, Cmd.none )
 
         ChangePassword password ->
-            ( model, Cmd.none )
+            ( { model | password = password }, Cmd.none )
 
         Submit ->
-            ( model, Cmd.none )
+            ( model, loginRequest model )
+
+        GotLoginResponse response ->
+            ( { model | loginResponse = response }, Cmd.none )
 
 
 init : Model
 init =
-    { username = "", password = "" }
+    { username = "", password = "", loginResponse = RemoteData.NotAsked }
